313223b625a248bc4c9a170d2fcf41ddcd75fc2e
banamine <banamine@gmail.com>
2025-11-23 08:59:04 -0800

    Remove large media files and add to gitignore

--- attached_assets/background_1763262645893.js
+++
@@ -1,8 +0,0 @@
-<<<<<<< HEAD
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL('player.html') });
-=======
-chrome.action.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.runtime.getURL('player.html') });
->>>>>>> 1a45e5f4554c44fc0223c1b77be694ab7eabb3b2
-});
--- attached_assets/push_to_github.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "C:\Users\banamine\Videos\M3U MATRIX ALL IN ONE\Liberty-Express-"
-git add -A
-git commit -m "Add all pages"
-git push origin main
-pause
